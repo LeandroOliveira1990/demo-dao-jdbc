6fa8ec867102d6fa54d9bbeaff58e6f61b809681
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-08-09 20:34:15 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/demo/dao/jdbc/DemoDaoJdbc.java
+++ demo-dao-jdbc/src/demo/dao/jdbc/DemoDaoJdbc.java
@@ -5,6 +5,8 @@
 package demo.dao.jdbc;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -23,6 +25,8 @@ public class DemoDaoJdbc {
         Department obj = new Department(1, "Books");
         
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        SellerDao sellerdao = DaoFactory.createSellerDao();
         
         System.out.println(seller);
     }
