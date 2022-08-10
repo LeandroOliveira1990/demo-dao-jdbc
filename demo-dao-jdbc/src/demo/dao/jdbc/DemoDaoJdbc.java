9042ef2faf86232a92fc2a3fdf6b81641af27cd8
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-08-09 21:51:21 -0300

    findAll implementation

--- demo-dao-jdbc/src/demo/dao/jdbc/DemoDaoJdbc.java
+++ demo-dao-jdbc/src/demo/dao/jdbc/DemoDaoJdbc.java
@@ -39,6 +39,14 @@ public class DemoDaoJdbc {
         for (Seller seller1 : list) {
             System.out.println(seller1);
         }
+        
+        
+         System.out.println("\n=== Test 3: seller findAll ===");
+         list = sellerdao.findAll();
+        
+        for (Seller seller1 : list) {
+            System.out.println(seller1);
+        }
     }
     
 }
